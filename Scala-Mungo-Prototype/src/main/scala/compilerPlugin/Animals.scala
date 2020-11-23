15c6e2bee94c7b06c1fde478f49d7dc6fad02dca
2206245r <2206245r@student.gla.ac.uk>
2020-11-23 16:55:31 +0000

    Adds data to look at in animals

--- Scala-Mungo-Prototype/src/main/scala/compilerPlugin/Animals.scala
+++ Scala-Mungo-Prototype/src/main/scala/compilerPlugin/Animals.scala
@@ -22,18 +22,22 @@ class MoneyStash() {
 @Typestate("CWeirdProtocol")
 class Cat{
   var friend:Cat = null
+  var friendToo:Cat = new Cat()
+  var friend3 = new Cat()
+  var friend4:Cat = _
   def walk(): Boolean = true
   def walkFriend(): Unit ={
     friend.walk()
   }
   def setFriend(f:Cat): Unit ={
-    friend = f
+    friend4 = f
   }
 }
 
 object Main extends App{
   val cat1 = new Cat()
   val cat2 = new Cat()
+  cat1.friend = cat2
   cat1.setFriend(cat2)
   cat1.walkFriend()
   cat2.walk()
